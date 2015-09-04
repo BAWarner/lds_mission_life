559de0b96d5e645e83d94459d512ae6b959c1fd9
Mike Payne <payne@rivetal.com>
2015-09-04 09:11:11 -0700

    adds feedback to user during vagrant up shell scripting

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,4 @@
+echo "Creating local wp config file..."
 cp /var/www/config/env/local-sample.php /var/www/config/env/local.php
+echo "Importing MySQL Database..."
 mysql -u root --password=root scotchbox < /var/www/config/db.sql
